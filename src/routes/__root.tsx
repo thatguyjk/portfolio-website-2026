446b15a5ae85b61d1c00ee7594a0957a619d2312
thatguyjk <jonathank.kelly@gmail.com>
2026-03-13 22:47:24 -0400

    adding global  404 route

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -33,7 +33,7 @@ export const Route = createRootRouteWithContext<MyRouterContext>()({
         content: 'width=device-width, initial-scale=1',
       },
       {
-        title: 'TanStack Start Starter',
+        title: 'The portfolio of Jonathan Kelly',
       },
     ],
     links: [
